5ce0ae887540302718d4d0cb02e48ea1d3e861d7
Kateryna Navozenko <katrinelle19@gmail.com>
2025-02-05 22:05:33 +0200

    Add files via upload

---
+++ HomeWork/js/script.js
@@ -0,0 +1,29 @@
+// Домашнє завдання
+// ----1----
+// Створіть функцію-конструктор Calculator,
+// який створює об’єкти з трьома методами:
+// read() запитує два значення за допомогою prompt і запам’ятовує
+// їх у властивостях об’єкта.
+// sum() повертає суму цих властивостей.
+// mul() повертає результат множення даних властивостей.
+
+// function Calculator() {
+//   this.read = function () {
+//     this.valueFirst = parseFloat(prompt("Enter the first number"));
+//     this.valueSecond = parseFloat(prompt("Enter the second number"));
+//     return alert((sum = this.valueFirst + this.valueSecond));
+//   };
+//   this.sum = function (valueFirst, valueSecond) {
+//     return alert((sum = this.valueFirst + this.valueSecond));
+//   };
+
+//   this.mul = function (valueFirst, valueSecond) {
+//     return alert((calc = this.valueFirst * this.valueSecond));
+//   };
+// }
+
+// let calculator = new Calculator();
+
+// calculator.read();
+// calculator.sum();
+// calculator.mul();
--- Lesson/js/script.js
+++ Lesson/js/script.js
@@ -1,16 +1,62 @@
+// -----
+
+// function User(name) {
+//   this.name = name;
+//   this.isAdmin = false;
+// }
+
+// let user = new User("Jeck");
+
+// alert(user.name); // Jeck
+// alert(user.isAdmin); // false
+
+// -----
+
+// function User(name) {
+//   this.name = name;
+//   this.sayHi = function () {
+//     alert("My name is: " + this.name);
+//   };
+// }
+
+// let ivan = new User("Ivan");
+// ivan.sayHi(); // вспливаюче вікно (тому що є alert) з текстом
+// // y name is: Ivan
+
+// -----
+
+// let obj = {};
+
+// let key = prompt("Введіть ключ", "_proto_");
+// obj[key] = "повне значення";
+
+// alert(obj[key]);
+
+// -----
+
+// let obj = Object.create(null);
+
+// let key = prompt("Введіть ключ", "_proto_");
+// obj[key] = "певне значення";
+
+// alert(obj[key]);
+
+// -----
+// // робимо кола різнокольорові и видкріваємо їх
+
 // class CircleBox {
 //   constructor(selector) {
 //     this.$el = document.querySelector(selector);
 //   }
-
 //   hide() {
 //     this.$el.style.display = "none";
 //   }
-
 //   show() {
 //     this.$el.style.display = "block";
 //   }
 // }
+
+// // extends CircleBox - extends - функція наслідцвання CircleBox - це звідки
 
 // class CircleItem extends CircleBox {
 //   constructor(options) {
@@ -40,29 +86,40 @@
 //   size: 30,
 // });
 
+// // в консолі на сторінці пишемо CircleBlack.hide() - чорний круг ховається
+
+// -----
+
 // function Car(name, color) {
 //   this.name = name;
 //   this.color = color;
 // }
 
+// // додаємо методи через прототип
 // Car.prototype.message = function () {
 //   console.log(`${this.name} is ${this.color} color`);
 // };
 
 // Car.prototype.start = function () {
-//   console.log(`${this.name} is start!!!`);
+//   console.log(`${this.name} is start`);
 // };
+// // // в консолі на сторінці пишемо opel.start() - opel is start
 
 // const BMW = new Car("bmw", "red");
+
 // const opel = new Car("opel", "green");
+
+// // в консолі на сторінці пишемо BMW.message() - bmw is red color
+
+// -----
+// mixin
 
 // let message = {
 //   messageHello() {
-//     console.log(`Hello, ${this.name}`);
+//     console.log(`Hello`, `${this.name}`);
 //   },
-
-//   messageBey() {
-//     console.log(`Bye, ${this.name}`);
+//   messageBye() {
+//     console.log(`Bye`, `${this.name}`);
 //   },
 // };
 
@@ -74,4 +131,30 @@
 
 // Object.assign(User.prototype, message);
 
-// new User("Іван").messageHello();
+// new User("Ivan").messageHello(); // Hello Ivan
+
+// -----
+
+// JS 2020 року:
+// height = height ?? 100;
+// // на старій версії:
+// height = height !== undefined && height !== null ? height : 100;
+
+// -----
+// Поліфіли – вбудовані функції
+
+// Math.trunc;
+// перевіряємо чі є ця функція
+
+// if (!Math.trunc)
+
+// правильні коментарі:
+
+// @paran {number} X число
+// @param {number} N піднесе в стіпінь
+
+// if (a === 5) {
+//   console.log("ok");
+// }
+
+// https://eslint.org/
